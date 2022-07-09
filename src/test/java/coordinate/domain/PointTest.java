0a83f94d500215be6970b40f3179a49b6e767c44
dunowljj <uuufg3322@gmail.com>
2022-07-09 13:20:17 +0900

    test : 입력받은 좌표가 0-24를 벗어나면 예외 던지는 테스트 올바르게 수정

--- src/test/java/coordinate/domain/PointTest.java
+++ src/test/java/coordinate/domain/PointTest.java
@@ -1,9 +1,10 @@
 package coordinate.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class PointTest {
 
@@ -16,23 +17,23 @@ public class PointTest {
         Point point = new Point(input);
 
         //then
-        Assertions.assertThat(point.getX()).isEqualTo(1);
-        Assertions.assertThat(point.getY()).isEqualTo(2);
+        assertThat(point.getX()).isEqualTo(1);
+        assertThat(point.getY()).isEqualTo(2);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"fS","(1,3","(1)","(13)"})
+    @ValueSource(strings = {"fS","(1,3","(1)","(13)","(-1,3)"})
     void Point_입력받기_잘못된_입력형식(String input) {
-        Assertions.assertThatThrownBy(() -> new Point(input))
+        assertThatThrownBy(() -> new Point(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력 형식이 잘못되었습니다.");
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"(0,-1)","(0,-1)","(25,24)","(24,25)"})
-    void Point_입력받기_0에서24(String input) {
-        Assertions.assertThatThrownBy(() -> new Point(input))
+    @ValueSource(strings = {"(0,28)","(0,30)","(25,24)","(24,25)"})
+    void Point_입력받기_0에서24_벗어나면_예외(String input) {
+        assertThatThrownBy(() -> new Point(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("입력 형식이 잘못되었습니다.");
+                .hasMessageContaining("0-24 범위의 좌표만 입력가능합니다.");
     }
 }
