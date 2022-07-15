892f087d5e819507838c32ffdd480f25225296b3
dunowljj <uuufg3322@gmail.com>
2022-07-15 17:41:01 +0900

    feat : 사각형 넓이를 구하는 기능

--- src/main/java/coordinate/model/Rectangle.java
+++ src/main/java/coordinate/model/Rectangle.java
@@ -1,9 +1,8 @@
 package coordinate.model;
 
-import java.security.Provider;
+import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Rectangle extends AbstractFigure {
@@ -17,8 +16,13 @@ public class Rectangle extends AbstractFigure {
         }
     }
     private boolean isRectangle() {
-        return getOnlySet(Point::getX).size() == 2
-                && getOnlySet(Point::getY).size() == 2;
+        return getOnlyXList().size() == 2 &&  getOnlyYList().size() == 2;
+    }
+    private List<Double> getOnlyXList() {
+        return getOnlySet(Point::getX).stream().collect(Collectors.toList());
+    }
+    private List<Double> getOnlyYList() {
+        return getOnlySet(Point::getY).stream().collect(Collectors.toList());
     }
     private Set<Double> getOnlySet(Function<Point,Double> function) {
         return getPoints().stream()
@@ -26,9 +30,14 @@ public class Rectangle extends AbstractFigure {
                 .collect(Collectors.toSet());
     }
 
-
     @Override
     public double area() {
-        return 0;
+        return getMinusX() * getMinusY();
+    }
+    private double getMinusX() {
+        return Math.abs(getOnlyXList().get(0) - getOnlyXList().get(1));
+    }
+    private double getMinusY() {
+        return Math.abs(getOnlyYList().get(0) - getOnlyYList().get(1));
     }
 }
--- src/test/java/coordinate/model/RectangleTest.java
+++ src/test/java/coordinate/model/RectangleTest.java
@@ -1,17 +1,30 @@
 package coordinate.model;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class RectangleTest {
     @Test
-    void 직사각형아니면_예외() {
+    void 직사각형_아니면_예외() {
         //given
         String input = "(3,5)-(7,5)-(1,3)-(7,3)";
 
         //when, then
-        Assertions.assertThatThrownBy(() -> new Rectangle(new Points(input)))
+        assertThatThrownBy(() -> new Rectangle(new Points(input)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("직사각형이 아닙니다.");
+    }
+
+    @Test
+    void 직사각형_녋이_구하기() {
+        //given
+        String input = "(1,5)-(7,5)-(1,3)-(7,3)";
+
+        //when
+        Rectangle rectangle = new Rectangle(new Points(input));
+
+        //then
+        assertThat(rectangle.area()).isEqualTo(12);
     }
 }
